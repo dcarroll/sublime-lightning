c92f1b7024977faf7d6a4da479e789c60a850ff6
Dave Carroll <djcarroll@gmail.com>
2014-07-10 13:55:33 -0700

    add sublime test 2 install

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
-#SUBLIME_DIR=~/Library/Application\ Support/Sublime\ Text\ 2/Packages/
 SUBLIME_DIR=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+SUBLIME2_DIR=~/Library/Application\ Support/Sublime\ Text\ 2/Packages/
 
+mkdir -p "$SUBLIME2_DIR"
 mkdir -p "$SUBLIME_DIR"
 
+cp -r "./Aura" "$SUBLIME2_DIR/"
 cp -r "./Aura" "$SUBLIME_DIR/"
 
+cat ./Aura/aura.sublime-build | sed "s#"'${path}'"#$PATH#"  > "$SUBLIME2_DIR/Aura/aura.sublime-build"
 cat ./Aura/aura.sublime-build | sed "s#"'${path}'"#$PATH#"  > "$SUBLIME_DIR/Aura/aura.sublime-build"
 
 echo "Aura Sublime Plugin install finished"
