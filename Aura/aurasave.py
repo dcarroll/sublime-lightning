9adc8f23da469f6698b6e52801869b249c144b44
Dave Carroll <djcarroll@gmail.com>
2014-07-14 12:30:30 -0700

    add on save publish

---
+++ Aura/aurasave.py
@@ -0,0 +1,18 @@
+import sublime, sublime_plugin, os, subprocess
+ 
+class AuraSave(sublime_plugin.EventListener):
+ 
+  def on_post_save(self, view):
+    print 'BuildonSave: on_post_save'
+
+    #let's see if project wants to be autobuilt.
+    should_build = view.settings().get('build_on_save')
+    filename = view.file_name()
+    if os.path.exists(os.path.dirname(filename)+'/.manifest'):
+      print os.path.dirname(filename)
+
+      command = '-e=' + filename
+      view.window().run_command('exec',{ 'cmd':["force", "pushAura", command] })
+      print 'Command should have executed'
+    else:
+      print 'BuildonSave: Project not configured for build_on_save.  Try setting build_on_save in project settings'
