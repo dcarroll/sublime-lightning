bf4bca718a5d52bb752d32664e750d2345cc1e1f
Dave Carroll <djcarroll+git@gmail.com>
2014-12-18 15:39:00 -0800

    Static resources

--- lightningsave.py
+++ lightningsave.py
@@ -3,6 +3,7 @@ import sublime_plugin
 import os
 import subprocess
 import json
+from walk_up import walk_up
 
 
 class Helper(sublime_plugin.WindowCommand):
@@ -26,6 +27,13 @@ class Helper(sublime_plugin.WindowCommand):
 
     def is_metadata(self, working_dir):
         return os.path.basename(os.path.dirname(working_dir)) == "metadata"
+
+    def is_static_resource(self, file):
+        for root, dirs, files in walk_up.walk_up(file):
+            if "staticresources" in dirs:
+                return True
+
+        return False
 
     def is_bundle_type(self, working_dir, comp_type):
         files = os.listdir(working_dir[0])
@@ -271,10 +279,12 @@ class LightningNewSvgCommand(sublime_plugin.WindowCommand):
             'xmlns:xlink="http://www.w3.org/1999/xlink">\n'
             '\t<g stroke="none" stroke-width="1" fill="none" '
             'fill-rule="evenodd">\n'
-            '\t\t<path d="M120,108 C120,114.6 114.6,120 108,120 L12,120 C5.4,120 '
+            '\t\t<path d="M120,108 C120,114.6 114.6,120 108,120 L12,120 '
+            'C5.4,120 '
             '0,114.6 0,108 L0,12 C0,5.4 5.4,0 12,0 L108,0 C114.6,0 120,5.4 '
             '120,12 L120,108 L120,108 Z" id="Shape" fill="#2A739E"/>\n'
-            '\t\t<path d="M77.7383308,20 L61.1640113,20 L44.7300055,63.2000173 '
+            '\t\t<path d="M77.7383308,20 L61.1640113,20 '
+            'L44.7300055,63.2000173 '
             'L56.0543288,63.2000173 L40,99.623291 L72.7458388,54.5871812 '
             'L60.907727,54.5871812 L77.7383308,20 Z" id="Path-1" '
             'fill="#FFFFFF"/>\n'
@@ -487,8 +497,14 @@ class LightningSave(sublime_plugin.EventListener):
                 'exec',
                 {'cmd': ["force", "pushAura", command]})
         elif Helper.is_metadata(self, os.path.dirname(filename)):
-            command = '-f=' + filename
-            view.window().run_command(
-                'exec',
-                {'cmd': ["force", "push", command]})
+            if Helper.is_static_resource(self, filename):
+                #comment
+                print("SR")
+            else:
+                command = '-f=' + filename
+                view.window().run_command(
+                    'exec',
+                    {'cmd': ["force", "push", command]})
+        elif Helper.is_static_resource(self, os.path.dirname(filename)):
+            print("This is an upacked static resource file.")
         return
---
+++ walk_up.py
@@ -0,0 +1,50 @@
+import os
+from os import path
+
+
+def walk_up(bottom):
+    """
+    mimic os.walk, but walk 'up'
+    instead of down the directory tree
+    """
+    bottom = path.realpath(bottom)
+
+    #get files in current dir
+    try:
+        names = os.listdir(bottom)
+    except Exception as e:
+        print(e)
+        return
+
+    dirs, nondirs = [], []
+    for name in names:
+        if path.isdir(path.join(bottom, name)):
+            dirs.append(name)
+        else:
+            nondirs.append(name)
+
+    yield bottom, dirs, nondirs
+
+    new_path = path.realpath(path.join(bottom, '..'))
+
+    # see if we are at the top
+    if new_path == bottom:
+        return
+
+    for x in walk_up(new_path):
+        yield x
+
+if __name__ == '__main__':
+    # tests/demos
+
+    #print all files and directories
+    # directly above the current one
+    for i in walk_up(os.curdir):
+        print(i)
+
+    # look for a TAGS file above the
+    # current directory
+    for c, d, f in walk_up(os.curdir):
+        if 'TAGS' in f:
+            print(c)
+            break
