b6b147353b02b1642c5cf1819a0a39fca7ad10f3
Dave Carroll <djcarroll+git@gmail.com>
2014-12-21 15:25:41 -0800

    add preview lighting

--- lightningsave.py
+++ lightningsave.py
@@ -83,7 +83,7 @@ class Helper(sublime_plugin.WindowCommand):
                              stderr=subprocess.PIPE)
         out, err = p.communicate()
         print(out)
-        data=json.load(out.decode("utf-8"))
+        data=json.load(out)
         return data["instanceUrl"]
 
     def get_app_name(self, adir):
