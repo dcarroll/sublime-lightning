807bcd7ef3c82ce60e6e0da448c49eac78d14149
Dave Carroll <djcarroll@gmail.com>
2015-10-14 12:02:26 -0700

    fetch result

--- lightningsave.py
+++ lightningsave.py
@@ -3,6 +3,7 @@ import sublime_plugin
 import os
 import subprocess
 import json
+import sys
 from . import semver
 
 
@@ -318,6 +319,7 @@ class Helper(sublime_plugin.WindowCommand):
                                              self.open_selected_bundle,
                                              sublime.MONOSPACE_FONT)
             except:
+                print("error: " + sys.exc_info()[0])
                 return
 
     def make_bundle_file(self, file_name, extension, snippet, dirs):
