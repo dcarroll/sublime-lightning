11ece2b8cce63ca499c623dc52eba30f943dd255
Dave Carroll <djcarroll@gmail.com>
2016-03-07 17:07:50 -0800

    aura

--- lightningsave.py
+++ lightningsave.py
@@ -239,6 +239,8 @@ class Helper(sublime_plugin.WindowCommand):
         if err:
             sublime.error_message(err.decode("utf-8"))
         else:
+            results = results.replace("\"", "'")
+            results = results.replace("\t", "")
             print(results)
 
     def get_forcecli_version(self):
